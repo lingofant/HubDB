78412b5c8482c739de4f51c6b93a8aaa9c39f5de
Timo <timo.lingnau@gmail.com>
2019-01-06 14:52:03 +0100

    added insert in root

--- DBLib/DBMyIndex.cpp
+++ DBLib/DBMyIndex.cpp
@@ -25,32 +25,21 @@ const BlockNo DBMyIndex::rootBlockNo(0);
 // max this amount of entries with the some value in the index - WHEN NOT UNIQUE
 const uint DBMyIndex::MAX_TID_PER_ENTRY(20);
 // Funktion bekannt machen
-extern "C" void * createDBMyIndex(int nArgs, va_list ap);
+extern "C" void *createDBMyIndex(int nArgs, va_list ap);
 
 int leaf_size = 10;
-
-union value_union{
-    int * child_leaf;
-    TID tid;
-};
-
-struct leaf {
-   leaf * smallest;
-   value_union values[10];
-};
-
 
 /**
  * Ausgabe des Indexes zum Debuggen
  */
 string DBMyIndex::toString(string linePrefix) const {
-	stringstream ss;
-	ss << linePrefix << "[DBMyIndex]" << endl;
-	ss << DBIndex::toString(linePrefix + "\t") << endl;
-	ss << linePrefix << "tidsPerEntry: " << tidsPerEntry << endl;
-	ss << linePrefix << "entriesPerPage: " << entriesPerPage() << endl;
-	ss << linePrefix << "-----------" << endl;
-	return ss.str();
+    stringstream ss;
+    ss << linePrefix << "[DBMyIndex]" << endl;
+    ss << DBIndex::toString(linePrefix + "\t") << endl;
+    ss << linePrefix << "tidsPerEntry: " << tidsPerEntry << endl;
+    ss << linePrefix << "entriesPerPage: " << entriesPerPage() << endl;
+    ss << linePrefix << "-----------" << endl;
+    return ss.str();
 }
 
 /** Konstruktor
@@ -60,40 +49,40 @@ string DBMyIndex::toString(string linePrefix) const {
  * - ModType mode (Accesstyp: READ, WRITE - siehe DBTypes.h)
  * - bool unique (ist Attribute unique)
  */
-DBMyIndex::DBMyIndex(DBBufferMgr & bufferMgr, DBFile & file,
-		enum AttrTypeEnum attrType, ModType mode, bool unique) :
-			// call base constructor
-			DBIndex(bufferMgr, file, attrType, mode, unique),
-			// wenn unqiue dann nur ein Tupel pro value, ansonsten den wert vom oben gesetzen MAX_TID_PER_ENTRY
-			tidsPerEntry(unique == true ? 1 : MAX_TID_PER_ENTRY),
-			// DBAttrType first_ und last_ = NULL
-			// diese beiden Werte dienen um den ersten und letzten Wert eines Blockes
-			// temporaer zwischenzuspeichern --> siehe genFirstLast()
-			//ToDo: Brauchen wir den?
-			first_(NULL), last_(NULL) {
-	if (logger != NULL) {
-		LOG4CXX_INFO(logger,"DBMyIndex()");
-	}
+DBMyIndex::DBMyIndex(DBBufferMgr &bufferMgr, DBFile &file,
+                     enum AttrTypeEnum attrType, ModType mode, bool unique) :
+// call base constructor
+        DBIndex(bufferMgr, file, attrType, mode, unique),
+        // wenn unqiue dann nur ein Tupel pro value, ansonsten den wert vom oben gesetzen MAX_TID_PER_ENTRY
+        tidsPerEntry(unique == true ? 1 : MAX_TID_PER_ENTRY),
+        // DBAttrType first_ und last_ = NULL
+        // diese beiden Werte dienen um den ersten und letzten Wert eines Blockes
+        // temporaer zwischenzuspeichern --> siehe genFirstLast()
+        //ToDo: Brauchen wir den?
+        first_(NULL), last_(NULL) {
+    if (logger != NULL) {
+        LOG4CXX_INFO(logger, "DBMyIndex()");
+    }
 
-	assert(entriesPerPage()>1);
+    assert(entriesPerPage() > 1);
 
-	//	if(unique == false && isIndexNonUniqueAble() == false)
-	//		throw HubDB::Exception::DBIndexException("set up nonunique but index does not support it");
+    //	if(unique == false && isIndexNonUniqueAble() == false)
+    //		throw HubDB::Exception::DBIndexException("set up nonunique but index does not support it");
 
-	// if this function is called for the first time -> index file has 0 blocks
-	// -> call initializeIndex to create file
-	if (bufMgr.getBlockCnt(file) == 0) {
-		LOG4CXX_DEBUG(logger,"initializeIndex");
-		initializeIndex();
-	}
+    // if this function is called for the first time -> index file has 0 blocks
+    // -> call initializeIndex to create file
+    if (bufMgr.getBlockCnt(file) == 0) {
+        LOG4CXX_DEBUG(logger, "initializeIndex");
+        initializeIndex();
+    }
 
-	// fuege ersten Block der Indexdatei in den Stack der geblockten Bloecke hinzu
-	bacbStack.push(bufMgr.fixBlock(file, rootBlockNo,
-			mode == READ ? LOCK_SHARED : LOCK_INTWRITE));
+    // fuege ersten Block der Indexdatei in den Stack der geblockten Bloecke hinzu
+    bacbStack.push(bufMgr.fixBlock(file, rootBlockNo,
+                                   mode == READ ? LOCK_SHARED : LOCK_INTWRITE));
 
-	if (logger != NULL) {
-		LOG4CXX_DEBUG(logger,"this:\n"+toString("\t"));
-	}
+    if (logger != NULL) {
+        LOG4CXX_DEBUG(logger, "this:\n" + toString("\t"));
+    }
 }
 
 /**
@@ -101,32 +90,32 @@ DBMyIndex::DBMyIndex(DBBufferMgr & bufferMgr, DBFile & file,
  * Soll first_, last_ und alle geblockten Bloecke wieder freigeben
  */
 DBMyIndex::~DBMyIndex() {
-	LOG4CXX_INFO(logger,"~DBMyIndex()");
-	unfixBACBs(false);
-	if (first_ != NULL)
-		delete first_;
-	if (last_ != NULL)
-		delete last_;
+    LOG4CXX_INFO(logger, "~DBMyIndex()");
+    unfixBACBs(false);
+    if (first_ != NULL)
+        delete first_;
+    if (last_ != NULL)
+        delete last_;
 }
 
 /**
  * Freigeben aller vom Index geblockten Bloecke
  */
 void DBMyIndex::unfixBACBs(bool setDirty) {
-	LOG4CXX_INFO(logger,"unfixBACBs()");
-	LOG4CXX_DEBUG(logger,"setDirty: "+TO_STR(setDirty));
-	LOG4CXX_DEBUG(logger,"bacbStack.size()= "+TO_STR(bacbStack.size()));
-	while (bacbStack.empty() == false) {
-		try {
-			if (bacbStack.top().getModified()) {
-				if (setDirty == true)
-					bacbStack.top().setDirty();
-			}
-			bufMgr.unfixBlock(bacbStack.top());
-		} catch (DBException e) {
-		}
-		bacbStack.pop();
-	}
+    LOG4CXX_INFO(logger, "unfixBACBs()");
+    LOG4CXX_DEBUG(logger, "setDirty: " + TO_STR(setDirty));
+    LOG4CXX_DEBUG(logger, "bacbStack.size()= " + TO_STR(bacbStack.size()));
+    while (bacbStack.empty() == false) {
+        try {
+            if (bacbStack.top().getModified()) {
+                if (setDirty == true)
+                    bacbStack.top().setDirty();
+            }
+            bufMgr.unfixBlock(bacbStack.top());
+        } catch (DBException e) {
+        }
+        bacbStack.pop();
+    }
 }
 
 /**
@@ -142,35 +131,50 @@ void DBMyIndex::unfixBACBs(bool setDirty) {
  * Bsp.: INTEGER UNIQUE: entriesPerPage() = (1024-4) / (4+8*1) = 85, Rest 0
  */
 uint DBMyIndex::entriesPerPage() const {
-	//ToDo: Ändern der entries per Page berechnung auf blätter
+    //ToDo: Ändern der entries per Page berechnung auf blätter
 
-	return (DBFileBlock::getBlockSize() - sizeof(uint)) /
-			(DBAttrType::getSize4Type(attrType) + sizeof(TID) * tidsPerEntry);
+    return (DBFileBlock::getBlockSize() - sizeof(uint)) /
+           (DBAttrType::getSize4Type(attrType) + sizeof(TID) * tidsPerEntry);
 }
 
 /**
  * Erstelle Indexdatei.
  */
 void DBMyIndex::initializeIndex() {
-	LOG4CXX_INFO(logger,"initializeIndex()");
-	if (bufMgr.getBlockCnt(file) != 0)
-		throw DBIndexException("can not initializie exisiting table");
+    LOG4CXX_INFO(logger, "initializeIndex()");
+    if (bufMgr.getBlockCnt(file) != 0)
+        throw DBIndexException("can not initializie exisiting table");
 
-	try {
-		// einen ersten Block der Datei erhalten und gleich fixen
-		bacbStack.push(bufMgr.fixNewBlock(file));
-		// modified date setzen
-		bacbStack.top().setModified();
-	} catch (DBException e) {
-		if (bacbStack.empty() == false)
-			bufMgr.unfixBlock(bacbStack.top());
-		throw e;
-	}
-	bufMgr.unfixBlock(bacbStack.top());
-	bacbStack.pop();
+    try {
+        // einen ersten Block der Datei erhalten und gleich fixen
+        bacbStack.push(bufMgr.fixNewBlock(file));
+        // modified date setzen
 
-	// nun muss die liste der geblockten Seiten wieder leer sein, sonst Abbruch
-	assert(bacbStack.empty()==true);
+        bool isroot = true;
+        bool isleaf = true;
+        bool *boolptr = &isroot;
+        char *ptr = bacbStack.top().getDataPtr();
+        uint *cnt = (uint *) ptr;
+        if (*cnt == 0) {
+            // memcpy (*destination, *source, size);
+            memcpy(ptr, boolptr, sizeof(bool));
+            ptr += sizeof(bool);
+            memcpy(ptr, boolptr, sizeof(bool));
+            ptr += sizeof(bool);
+            int nextBlockNo = 15;
+            memcpy(ptr, &nextBlockNo, sizeof(nextBlockNo));
+        }
+        bacbStack.top().setModified();
+    } catch (DBException e) {
+        if (bacbStack.empty() == false)
+            bufMgr.unfixBlock(bacbStack.top());
+        throw e;
+    }
+    bufMgr.unfixBlock(bacbStack.top());
+    bacbStack.pop();
+
+    // nun muss die liste der geblockten Seiten wieder leer sein, sonst Abbruch
+    assert(bacbStack.empty() == true);
 }
 
 /**
@@ -180,43 +184,122 @@ void DBMyIndex::initializeIndex() {
  * Rueckgabewert: KEINER, aber die Funktion aendert die uebergebene Liste
  * von TID Objekten (siehe DBTypes.h: typedef list<TID> DBListTID;)
  */
-void DBMyIndex::find(const DBAttrType & val, DBListTID & tids) {
-	LOG4CXX_INFO(logger,"find()");
-	LOG4CXX_DEBUG(logger,"val:\n"+val.toString("\t"));
+void DBMyIndex::find(const DBAttrType &val, DBListTID &tids) {
+    LOG4CXX_INFO(logger, "find()");
+    LOG4CXX_DEBUG(logger, "val:\n" + val.toString("\t"));
 
-	// ein Block muss geblockt sein
-	if (bacbStack.size() != 1)
-		throw DBIndexException("BACB Stack is invalid");
+    // ein Block muss geblockt sein
+    if (bacbStack.size() != 1)
+        throw DBIndexException("BACB Stack is invalid");
 
-	// Löschen der uebergebenen Liste ("Returnliste")
-	tids.clear();
+    // Löschen der uebergebenen Liste ("Returnliste")
+    tids.clear();
 
-	//ToDo: Implement Search in Tree and fill List tids
+    //ToDo: Implement Search in Tree and fill List tids
 }
 
 /**
  * Einfuegen eines Schluesselwertes (moeglicherweise bereits vorhangen)
  * zusammen mit einer Referenz auf eine TID.
  */
-void DBMyIndex::insert(const DBAttrType & val, const TID & tid) {
-	LOG4CXX_INFO(logger,"insert()");
-	LOG4CXX_DEBUG(logger,"val:\n"+val.toString("\t"));
-	LOG4CXX_DEBUG(logger,"tid: "+tid.toString());
+void DBMyIndex::insert(const DBAttrType &val, const TID &tid) {
+    LOG4CXX_INFO(logger, "insert()");
+    LOG4CXX_DEBUG(logger, "val:\n" + val.toString("\t"));
+    LOG4CXX_DEBUG(logger, "tid: " + tid.toString());
 
-	// vor Beginn der Operation darf nur genau eine Seite
-	// (wie immer die nullte) gelockt sein
-	if (bacbStack.size() != 1)
-		throw DBIndexException("BACB Stack is invalid");
+    // vor Beginn der Operation darf nur genau eine Seite
+    // (wie immer die nullte) gelockt sein
+    //ToDo: Wahrscheinlich ändern
 
-	// wenn diese eine Seite nicht exclusive gelockt ist, dann dies nachholen!
-	if (bacbStack.top().getLockMode() != LOCK_EXCLUSIVE)
-		bufMgr.upgradeToExclusive(bacbStack.top());
+    if (bacbStack.size() != 1)
+        throw DBIndexException("BACB Stack is invalid");
 
-	//ToDo: implementieren von Suche der passenden Leaf und insert Operation - dabei split berücksichtigen
 
-	// am Ende der operation muss genau eine Seite gelockt sein
-	if (bacbStack.size() != 1)
-		throw DBIndexException("BACB Stack is invalid");
+
+
+    // wenn diese eine Seite nicht exclusive gelockt ist, dann dies nachholen!
+    if (bacbStack.top().getLockMode() != LOCK_EXCLUSIVE)
+        bufMgr.upgradeToExclusive(bacbStack.top());
+
+
+    //ToDo: implementieren von Suche der passenden Leaf und insert Operation - dabei split berücksichtigen
+
+    /*
+     * springe zu 2*boolen und 1*pointer zu erstem value
+     * füge ein
+     */
+
+
+
+
+    char *ptr = bacbStack.top().getDataPtr();
+    bool isroot;
+    // memcpy (*destination, *source, size);
+    memcpy(&isroot, ptr, sizeof(bool));
+    ptr += sizeof(bool);
+
+    bool isleaf;
+    memcpy(&isleaf, ptr, sizeof(bool));
+    ptr += sizeof(bool);
+
+    int nextBlockNo;
+    memcpy(&nextBlockNo, ptr, sizeof(nextBlockNo));
+    ptr += sizeof(nextBlockNo);
+
+
+    // memcpy (*destination, *source, size);
+
+    uint *cnt = (uint *) ptr;
+
+    DBAttrType *searchval;
+    char *insert_position, *end_position;
+
+
+    bool found = false;
+    for (int i = 0; i < leaf_size; ++i) {
+        if (*cnt == 0) {
+            end_position = ptr;
+            break;
+        }
+        else{
+            searchval = DBAttrType::read(ptr, attrType);
+            if (val.operator<(*searchval) && !found) {
+                found = true;
+                insert_position = ptr;
+            }
+        }
+        ptr += (sizeof(val) + sizeof(tid));
+        cnt = (uint *) ptr;
+
+    }
+    if (found) {
+        //ToDo: Folgendes geht nur wenn man weiß das in dieses blatt eingefügt wird
+        // ist dies nicht der fall muss man schauen ob das nächste blatt einen höreren wert hat
+        do {
+            char *swap_pointer = end_position;
+            end_position -= sizeof(tid);
+            memcpy(swap_pointer + sizeof(val), end_position, sizeof(tid));
+            end_position -= sizeof(val);
+            memcpy(swap_pointer, end_position, sizeof(val));
+        } while (end_position != insert_position);
+
+    }
+    val.write(end_position);
+    end_position += sizeof(val);
+    tid.write(end_position);
+
+    // memcpy (*destination, *source, size);
+    bacbStack.top().setModified();
+    bufMgr.unfixBlock(bacbStack.top());
+
+    last_ = DBAttrType::read(ptr, attrType);
+
+
+
+
+    // am Ende der operation muss genau eine Seite gelockt sein
+    if (bacbStack.size() != 1)
+        throw DBIndexException("BACB Stack is invalid");
 }
 
 /**
@@ -224,39 +307,40 @@ void DBMyIndex::insert(const DBAttrType & val, const TID & tid) {
  * Um schneller auf der richtigen Seite mit dem Entfernen anfangen zu koennen,
  * wird zum Suchen auch noch der zu loeschende value uebergeben
  */
-void DBMyIndex::remove(const DBAttrType & val, const list<TID> & tid) {
-	LOG4CXX_INFO(logger,"remove()");
-	LOG4CXX_DEBUG(logger,"val:\n"+val.toString("\t"));
+void DBMyIndex::remove(const DBAttrType &val, const list<TID> &tid) {
+    LOG4CXX_INFO(logger, "remove()");
+    LOG4CXX_DEBUG(logger, "val:\n" + val.toString("\t"));
 
-	// vor Beginn der Operation darf nur genau eine Seite (wie immer die nullte) gelockt sein
-	if (bacbStack.size() != 1)
-		throw DBIndexException("BACB Stack is invalid");
+    // vor Beginn der Operation darf nur genau eine Seite (wie immer die nullte) gelockt sein
+    if (bacbStack.size() != 1)
+        throw DBIndexException("BACB Stack is invalid");
 
-	// wenn diese eine Seite nicht exclusive gelockt ist, dann dies nachholen!
-	if (bacbStack.top().getLockMode() != LOCK_EXCLUSIVE)
-		bufMgr.upgradeToExclusive(bacbStack.top());
+    // wenn diese eine Seite nicht exclusive gelockt ist, dann dies nachholen!
+    if (bacbStack.top().getLockMode() != LOCK_EXCLUSIVE)
+        bufMgr.upgradeToExclusive(bacbStack.top());
 
-	// wenn das Indexattribut unique ist, dann darf in der Liste TID nie mehr als ein Wert stehen
-	if (unique == true && tid.size() > 1)
-		throw DBIndexUniqueKeyException("try to remove multiple key but is unique index");
+    // wenn das Indexattribut unique ist, dann darf in der Liste TID nie mehr als ein Wert stehen
+    if (unique == true && tid.size() > 1)
+        throw DBIndexUniqueKeyException("try to remove multiple key but is unique index");
 
-	// Suchen auf welcher seite Tupel mit val im IndexAttribute liegen (wenn vorhanden)
-	// oder liegen koennten (wenn nicht vorhanden) und die in variable 'page' abspeichern
+    // Suchen auf welcher seite Tupel mit val im IndexAttribute liegen (wenn vorhanden)
+    // oder liegen koennten (wenn nicht vorhanden) und die in variable 'page' abspeichern
 
-	//ToDo: Implementierung von suche des leafs und löschen dieser - dabei merge berücksichtigen
+    //ToDo: Implementierung von suche des leafs und löschen dieser - dabei merge berücksichtigen
 
-	// am Ende der Operation muss genau eine Seite gelockt sein
-	if (bacbStack.size() != 1)
-		throw DBIndexException("BACB Stack is invalid");
+    // am Ende der Operation muss genau eine Seite gelockt sein
+    if (bacbStack.size() != 1)
+        throw DBIndexException("BACB Stack is invalid");
 }
 
 /**
  * Fuegt createDBMyIndex zur globalen factory method-map hinzu
  */
 int DBMyIndex::registerClass() {
-	setClassForName("DBMyIndex", createDBMyIndex);
-	return 0;
+    setClassForName("DBMyIndex", createDBMyIndex);
+    return 0;
 }
+
 
 /**
  * Gerufen von HubDB::Types::getClassForName von DBTypes, um DBIndex zu erstellen
@@ -266,15 +350,15 @@ int DBMyIndex::registerClass() {
  * - ModeType: READ, WRITE
  * - bool: unique Indexattribut
  */
-extern "C" void * createDBMyIndex(int nArgs, va_list ap) {
-	// Genau 5 Parameter
-	if (nArgs != 5) {
-		throw DBException("Invalid number of arguments");
-	}
-	DBBufferMgr * bufMgr = va_arg(ap,DBBufferMgr *);
-	DBFile * file = va_arg(ap,DBFile *);
-	enum AttrTypeEnum attrType = (enum AttrTypeEnum) va_arg(ap,int);
-	ModType m = (ModType) va_arg(ap,int);
-	bool unique = (bool) va_arg(ap,int);
-	return new DBMyIndex(*bufMgr, *file, attrType, m, unique);
+extern "C" void *createDBMyIndex(int nArgs, va_list ap) {
+    // Genau 5 Parameter
+    if (nArgs != 5) {
+        throw DBException("Invalid number of arguments");
+    }
+    DBBufferMgr *bufMgr = va_arg(ap, DBBufferMgr *);
+    DBFile *file = va_arg(ap, DBFile *);
+    enum AttrTypeEnum attrType = (enum AttrTypeEnum) va_arg(ap, int);
+    ModType m = (ModType) va_arg(ap, int);
+    bool unique = (bool) va_arg(ap, int);
+    return new DBMyIndex(*bufMgr, *file, attrType, m, unique);
 }
--- include/hubDB/DBMyIndex.h
+++ include/hubDB/DBMyIndex.h
@@ -3,47 +3,49 @@
 
 #include <hubDB/DBIndex.h>
 
-namespace HubDB{
-	namespace Index{
-		class DBMyIndex : public DBIndex{
-			
-			public:
-				DBMyIndex(DBBufferMgr & bufferMgr,DBFile & file,enum AttrTypeEnum attrType,ModType mode,bool unique);
-				~DBMyIndex();
-				string toString(string linePrefix="") const;
-				
-				void initializeIndex();
-				void find(const DBAttrType & val,DBListTID & tids);
-				void insert(const DBAttrType & val,const TID & tid);
-				void remove(const DBAttrType & val,const DBListTID & tid);
-				bool isIndexNonUniqueAble(){ return false;};
-				void unfixBACBs(bool dirty);				
-				
-				static int registerClass();
-				
-			private:
-				static const uint MAX_TID_PER_ENTRY;
-				const uint tidsPerEntry; 
-				bool findFirstPage(const DBAttrType & val,BlockNo & blockNo);
-				uint entriesPerPage()const;
-				void genFirstLast(const char * ptr);
-				DBAttrType & first() { return *first_;};
-				DBAttrType & last() { return *last_;};
-				
-				bool isEmpty(const char * ptr);
-                void insertEmptyPage(BlockNo pos);
-                void removeEmptyPage(BlockNo pos);
-                void insertInPage(const DBAttrType & val,const TID & tids,BlockNo pos);
-                void findFromPage(const DBAttrType & val,BlockNo pos,list<TID> & tids);
-                void removeFromPage(const DBAttrType & val,list<TID> tid,BlockNo pos);
-			
-	  			static LoggerPtr logger;
-				static const BlockNo rootBlockNo;
-				stack<DBBACB> bacbStack;
-				DBAttrType * first_;
-				DBAttrType * last_;
-		};
-	}
+namespace HubDB {
+    namespace Index {
+        class DBMyIndex : public DBIndex {
+
+        public:
+            DBMyIndex(DBBufferMgr &bufferMgr, DBFile &file, enum AttrTypeEnum attrType, ModType mode, bool unique);
+
+            ~DBMyIndex();
+
+            string toString(string linePrefix = "") const;
+
+            void initializeIndex();
+
+            void find(const DBAttrType &val, DBListTID &tids);
+
+            void insert(const DBAttrType &val, const TID &tid);
+
+            void remove(const DBAttrType &val, const DBListTID &tid);
+
+            bool isIndexNonUniqueAble() { return false; };
+
+            void unfixBACBs(bool dirty);
+
+            static int registerClass();
+
+        private:
+            static const uint MAX_TID_PER_ENTRY;
+            const uint tidsPerEntry;
+
+
+            uint entriesPerPage() const;
+
+            DBAttrType &first() { return *first_; };
+
+            DBAttrType &last() { return *last_; };
+
+            static LoggerPtr logger;
+            static const BlockNo rootBlockNo;
+            stack<DBBACB> bacbStack;
+            DBAttrType *first_;
+            DBAttrType *last_;
+        };
+    }
 }
 
 
